d8344bbe8ff25d7d57645d5380559de1acb414aa
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-06-25 23:27:07 -0600

    html format almost complete, missing a few divs

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import Header from "./components/Header/Header"
+import Recent from "./components/Recents/Recent"
+
+import "./App.css"
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='page'>
+        <div>
+          <Header />
+        </div>
+
+        <div className='middle'>
+          <div className='left'> 
+            <form>
+              <input
+                placeholder='City name'
+              />
+
+              <input
+                placeholder='State Name'
+              />
+            </form>
+          </div>
+
+          <div className='right'>
+            <Recent />
+          </div>
+
+        </div>
+
       </div>
     )
   }
---
+++ weather-app-frontend/src/components/Recents/Recent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import "./Recent.css"
+
+export class Recent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Recently Searched Locations</h1>
+                <div>
+                    
+                </div>
+
+            </div>
+        )
+    }
+}
+
+// here i am going to render my backend searches.
+// map
+
+export default Recent
